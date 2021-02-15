c9e89ee9881d7f7122194a48c5314842ca0a2ba9
Marcin Lentner <marcin_len@yahoo.co.uk>
2021-02-15 21:44:45 +0000

    Command line argument handling with minimist

---
+++ command_line_scripts_with_arguments_using_minimist_argument_handling.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+"use strict";
+
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help"],
+  string: ["file"],
+});
+
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  console.log(args.file);
+} else {
+  error("Incorrect usage.", true);
+}
+
+// **********************
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
+
+function printHelp() {
+  console.log("ex1 usage:");
+  console.log("");
+  console.log("--help                      print this help");
+  console.log("-, --in                     read file from stdin");
+  console.log("--file={FILENAME}           read file from {FILENAME}");
+  console.log("");
+  console.log("");
+}
